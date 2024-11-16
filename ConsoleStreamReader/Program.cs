cff3a2fcfd968b4327d41e98cf53caa0743e044a
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-11-16 20:17:06 -0300

    Code refactor

---
+++ ConsoleStreamReader/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleStreamReader
+{
+   internal abstract class Program {
+        private static void Main() {
+            const string path = @"C:temp\FIle.txt";
+            var sr = File.OpenText(path);
+            try{
+                while (!sr.EndOfStream)
+                    Console.WriteLine(sr.ReadLine());
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                sr.Close();
+                sr.Dispose();
+            }
+        }
+    }
+}
